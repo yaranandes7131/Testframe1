58d74ae217fc97bdc1a78a3cdfd340bfb2372cf1
yaranandes7131 <124922751+yaranandes7131@users.noreply.github.com>
2023-03-02 14:56:05 -0300

    Add files via upload

---
+++ TextFieldFrametest.java
@@ -0,0 +1,16 @@
+
+import javax.swing.JFrame;
+
+public class TextFieldFrametest {
+    
+    public static void main( String [] args){
+        
+   
+        TextFieldFrame textFieldFrame = new TextFieldFrame();
+        textFieldFrame.setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE);
+        textFieldFrame.setSize( 350,100 );
+        textFieldFrame.setVisible ( true );
+        
+    }
+    
+}
